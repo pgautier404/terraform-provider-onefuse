14aebca84add8a9fe1ef8623b40880a78a626748
Joe Hitt <jhitt@sovlabs.com>
2019-07-24 13:10:18 -0400

    SOV-1712 add powerpoint, exploring IP assignment

--- doit.sh
+++ doit.sh
@@ -7,7 +7,7 @@ echo 'running terraform destroy (for prior vm)'
 terraform destroy
 
 # clean, build, setup environment, init and apply the Terraform provider
-rm -f terraform.tfstate
+rm -f terraform.tfstate*
 rm -rf /tmp/tf-state*
 rm /tmp/terraform-log
 echo 'building go plugin...'
