a822f98cc7b7ae0bc3065cdabbd87d240e6b3c00
Joe Hitt <jhitt@sovlabs.com>
2019-07-26 11:28:25 -0400

    SOV-1719 Update README.md and make TF_VARS consistent in the example

--- setenv.sh
+++ setenv.sh
@@ -3,12 +3,10 @@
 export TF_LOG="TRACE"
 export TF_LOG_PATH=terraform.log
 
-export TF_VAR_SOVLABS_ADDRESS="localhost"
-export TF_VAR_SOVLABS_PORT=8080
-export TF_VAR_SOVLABS_USER="root@sovlabs.local"
-export TF_VAR_SOVLABS_PASSWORD="password"
+export TF_VAR_sovlabs_address="localhost"
+export TF_VAR_sovlabs_port=8080
+export TF_VAR_sovlabs_user="root@sovlabs.local"
+export TF_VAR_sovlabs_password="password"
 
-export TF_VAR_VSPHERE_USER=<vsphere_user>
-export TF_VAR_VSPHERE_PASSWORD=<password>
-export TF_VAR_VSPHERE_SERVER=vcenter01.sovlabs.net
+export TF_VAR_vsphere_server=vcenter01.sovlabs.net
 
